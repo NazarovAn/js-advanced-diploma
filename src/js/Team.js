3a3a7a88cb29fb56c5fe36d369c33e198ea7929f
NazarovAn <naatonchrome@gmail.com>
2020-06-01 17:25:13 +0400

    executed Team class

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,3 +1,54 @@
-export default class Team {
+import { generateTeam, getRandomInt } from './generators';
+import Bowman from './characters/Bowman';
+import Swordsman from './characters/Swordsman';
+import Magician from './characters/Magician';
+import Daemon from './characters/Daemon';
+import Undead from './characters/Undead';
+import Vampire from './characters/Vampire';
 
+const playerCharactersTypes = new Set([
+  Bowman,
+  Swordsman,
+  Magician,
+]);
+
+const computerCharactersTypes = new Set([
+  Daemon,
+  Undead,
+  Vampire,
+]);
+
+function getPlayerPositions(team, membersCount) {
+  const intArray = [];
+  const resultSet = new Set();
+  let allowedLineA = 0;
+  let allowedLineB = 1;
+  if (team !== 'Player') {
+    allowedLineA = 7;
+    allowedLineB = 6;
+  }
+  for (let i = 0; i < 64; i += 1) {
+    if (i % 8 === allowedLineA || i % 8 === allowedLineB) {
+      intArray.push(i);
+    }
+  }
+  while (resultSet.size < membersCount) {
+    resultSet.add(intArray[getRandomInt(15, 0)]);
+  }
+  return Array.from(resultSet);
+}
+
+export default class Team {
+  constructor(teamType, maxLevel, charactersCount) {
+    this.type = teamType;
+    if (teamType === 'Player') {
+      this.members = generateTeam(playerCharactersTypes, maxLevel, charactersCount);
+    } else {
+      this.members = generateTeam(computerCharactersTypes, maxLevel, charactersCount);
+    }
+    if (charactersCount > 10) {
+      throw new Error('Team size must be less than 10');
+    }
+    this.startingPositions = getPlayerPositions(teamType, charactersCount);
+  }
 }
