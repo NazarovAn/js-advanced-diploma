245961ab734268728d872f1a53dbbd97b840023c
NazarovAn <naatonchrome@gmail.com>
2020-06-02 16:11:50 +0400

    executed character selection

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,5 +1,7 @@
 import Team from './Team';
 import { getPositionedCharacters } from './PositionedCharacter';
+import GameState from './GameState';
+import GamePlay from './GamePlay';
 
 export function getTooltipString(obj) {
   const icons = {
@@ -14,37 +16,57 @@ export function getTooltipString(obj) {
   return `${icons.level}${level} ${icons.attack}${attack} ${icons.defence}${defence} ${icons.health}${health}`;
 }
 
+function findCharacterOnCell(teamA, teamB, index) {
+  const aChar = teamA.members.find((char) => char.position === index);
+  if (aChar !== undefined) {
+    aChar.team = teamA.type;
+    return aChar;
+  }
+  const bChar = teamB.members.find((char) => char.position === index);
+  if (bChar !== undefined) {
+    bChar.team = teamB.type;
+    return bChar;
+  }
+  return null;
+}
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
     this.playerTeam = new Team('Player', 4, 4);
     this.computerTeam = new Team('Computer', 3, 3);
+    this.gameState = new GameState();
   }
 
   init() {
     this.gamePlay.drawUi('prairie');
     this.gamePlay.redrawPositions(getPositionedCharacters(this.playerTeam, this.computerTeam));
     this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
-    // this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
     this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
   }
 
   onCellClick(index) {
+    const character = findCharacterOnCell(this.playerTeam, this.computerTeam, index);
+    if (character.team !== this.gameState.turn) {
+      GamePlay.showError(`Ход команды ${this.gameState.turn}`);
+    } else {
+      const { selectedCell } = this.gameState;
+      if (selectedCell !== undefined) {
+        this.gamePlay.deselectCell(selectedCell);
+      }
+      this.gamePlay.selectCell(index);
+      this.gameState.selectedCell = index;
+    }
   }
 
   onCellEnter(index) {
-    const playerCharOnCell = this.playerTeam.members.find((char) => char.position === index);
-    const computerCharOnCell = this.computerTeam.members.find((char) => char.position === index);
-    let tooltipString = '';
-    if (playerCharOnCell !== undefined) {
-      tooltipString = getTooltipString(playerCharOnCell);
+    const character = findCharacterOnCell(this.playerTeam, this.computerTeam, index);
+    if (character !== null) {
+      const tooltipString = getTooltipString(character);
+      this.gamePlay.showCellTooltip(tooltipString, index);
     }
-    if (computerCharOnCell !== undefined) {
-      tooltipString = getTooltipString(computerCharOnCell);
-    }
-
-    this.gamePlay.showCellTooltip(tooltipString, index);
   }
 
   onCellLeave(index) {
--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,4 +1,7 @@
 export default class GameState {
+  constructor() {
+    this.turn = 'Player';
+  }
   static from(object) {
     // TODO: create object
     return null;
